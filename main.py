f59da4d781f9a7e2422d767bdc45f73bff1495d7
Pranav <rakasi@umich.edu>
2024-11-07 14:21:17 -0500

    Started Working on HumanPlayer

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from player import HumanPlayer, Player
-from card import Card, CardCombination, Deck
+from card import Card, CardCombination, Deck, Play
 
 
 class BigTwoGame:
     def __init__(self):
         self.deck = Deck()
         hands = self.deck.deal(4)
+        # self.players: list[Player] = [
+        #     HumanPlayer("You", hands[0]),
+        #     Player("Computer 1", hands[1]),
+        #     Player("Computer 2", hands[2]),
+        #     Player("Computer 3", hands[3]),
+        # ]
+        hands = self.deck.deal(2)
         self.players: list[Player] = [
-            HumanPlayer("You", hands[0]),
-            Player("Computer 1", hands[1]),
-            Player("Computer 2", hands[2]),
-            Player("Computer 3", hands[3]),
+            HumanPlayer("Player1", hands[0]),
+            HumanPlayer("Player2", hands[1]),
         ]
-        self.current_player_index = 0
+        # the current player index would be for the player holding the 3 of diamonds
+        # check for 3 of diamonds
+        for i in range(len(self.players)):
+            lowestCard = self.players[i].hand[0]
+            if(lowestCard.suit == 'Diamonds' and lowestCard.rank == '3'):
+                self.current_player_index = i
         self.last_play = None
-        self.current_combination = CardCombination.ANY
+        self.current_combination = CardCombination.SINGLE
 
     def next_player(self):
         self.current_player_index = (self.current_player_index + 1) % len(
@@ -24,11 +34,11 @@ class BigTwoGame:
     def play_round(self):
         player = self.players[self.current_player_index]
         print(f"\n{player.name}'s turn")
-
-        play = player.find_plays(self.last_play)
+        print("Current Combination:",self.current_combination.name)
+        play = player.find_plays(self.last_play, self.current_combination)
         if play:
             print(f"{player.name} plays: {play}")
-            self.last_play = [play]
+            self.last_play = play
         else:
             print(f"{player.name} passes")
 
